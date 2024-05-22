c8d9fc2c42f1d28d8a82d26c4c06cec2c2841caf
stormxdev2 <168479104+stormxdev2@users.noreply.github.com>
2024-05-22 23:20:41 +0300

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,19 @@
-var http = require('http');
+const http = require('http');
 
-http.createServer(function (req, res) {
-  res.write("I'm alive");
-  res.end();
-}).listen(8080);
+function startServer() {
+  const server = http.createServer((req, res) => {
+    res.write("I'm alive");
+    res.end();
+  });
+
+  server.listen(8080, () => {
+    console.log('Keep-alive server is running on port 8080');
+  });
+
+  server.on('error', (err) => {
+    console.error('Server error:', err);
+    setTimeout(startServer, 5000); // Restart the server after 5 seconds on error
+  });
+}
+
+startServer();
