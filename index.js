172a883dead97a4744dc4ce234ada9cf8b16a954
stormxdev2 <168479104+stormxdev2@users.noreply.github.com>
2024-05-31 00:17:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function attemptReconnect(reason) {
     reasonString = reason.value.translate;
   }
 
-  if (typeof reasonString === 'string' && reasonString.includes('throttled')) {
+  if (reasonString.includes('throttled')) {
     console.log('Throttled by server, waiting before next reconnect...');
     reconnectAttempts++;
     setTimeout(() => {
